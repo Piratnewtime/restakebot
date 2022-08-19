b69467f84476ec24def56ce8a7eb6df073dc6749
Pirate newTime <himyl@ya.ru>
2022-08-19 14:49:30 +0400

    update default cosmoshub public host

--- src/classes/wallets/Cosmos.ts
+++ src/classes/wallets/Cosmos.ts
@@ -4,7 +4,7 @@ import Proto from "./__proto__/cosmos_network/Cosmos_v044";
 export default class Cosmos extends Proto {
 	static defaultConfig (): DefaultConfig {
 		return {
-			host: 'https://lcd-cosmos.cosmostation.io/',
+			host: 'https://lcd-cosmoshub.whispernode.com/',
 			gasPrice: 0.01,
 			prefix: 'cosmos',
 			nativeDenom: 'uatom'
